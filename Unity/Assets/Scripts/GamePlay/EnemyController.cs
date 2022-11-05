4f23fcaf89431c028ed404a90631210073bf738d
Null <289040411@qq.com>
2022-11-05 15:45:27 +0800

    敌人检测

--- Unity/Assets/Scripts/GamePlay/EnemyController.cs
+++ Unity/Assets/Scripts/GamePlay/EnemyController.cs
@@ -1,4 +1,4 @@
-using System;
+using Game.GameEvent;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
@@ -8,45 +8,104 @@ namespace Game
     {
         [SerializeField] private Light2D _SightLight2D;
         [SerializeField, Header("距离")] private float _lookDistance = 1f;
-        [SerializeField, Header("角度")] private float _lookAngle = 45f;
-        [SerializeField, Header("射线数")] private int _lookCount = 2;
 
+        [SerializeField, Header("角度"), Range(30, 180f)]
+        private float _lookAngle = 45f;
+
+        [SerializeField, Header("射线数")] private int _lookCount = 5;
+
+
+        [SerializeField, Header("当前警戒值")] private int _currentAlertValue;
+
+        [SerializeField, Header("最高值,超过该值就报警")]
+        private int _maxAlertValue = 30;
+
+        [SerializeField, Header("没发现玩家的时候每秒减少的值")]
+        private int _reducePerSecondAlertValue = 1;
+
+        [SerializeField, Header("发现玩家的时候每秒增加的值")]
+        private int _addPerSecondAlertValue = -1;
+
+        [SerializeField] private LayerMask _layerMask;
+        public int EnemyId;
         private Transform _player;
+
+        private bool _isFoundPlayer;
+
+        private void OnValidate()
+        {
+            _SightLight2D.pointLightOuterAngle = _lookAngle;
+            _SightLight2D.pointLightOuterRadius = _lookDistance;
+        }
+
+        private void OnDrawGizmos()
+        {
+            //多一个精确度就多两条对称的射线,每条射线夹角是总角度除与精度
+            var subAngle = (_lookAngle / 2) / _lookCount;
+            for (int i = 0; i < _lookCount; i++)
+            {
+                var angleLeft = Quaternion.Euler(0, 0, -1 * subAngle * (i + 1));
+                var angleRight = Quaternion.Euler(0, 0, 1 * subAngle * (i + 1));
+                Debug.DrawRay(transform.position, angleLeft * transform.up.normalized * _lookDistance, Color.cyan);
+                Debug.DrawRay(transform.position, angleRight * transform.up.normalized * _lookDistance, Color.cyan);
+            }
+        }
 
         private void Awake()
         {
             _SightLight2D.pointLightOuterAngle = _lookAngle;
             _SightLight2D.pointLightOuterRadius = _lookDistance;
             _player = GameDataCache.Instance.Player.transform;
+
+            InvokeRepeating(nameof(EnemyDetectPerSec), 1f, 1f);
         }
 
         private void Update()
         {
-            Look();
-        }
+            _isFoundPlayer = false;
+            if (Vector3.Distance(_player.transform.position, transform.position) > _lookDistance)
+            {
+                return;
+            }
 
-        //放射线检测
-        private bool Look()
-        {
             //多一个精确度就多两条对称的射线,每条射线夹角是总角度除与精度
             var subAngle = (_lookAngle / 2) / _lookCount;
             for (int i = 0; i < _lookCount; i++)
             {
-                if (LookAround(Quaternion.Euler(0, -1 * subAngle * (i + 1), 0)) ||
-                    LookAround(Quaternion.Euler(0, subAngle * (i + 1), 0)))
-                    return true;
+                var angleLeft = Quaternion.Euler(0, 0, -1 * subAngle * (i + 1));
+                var angleRight = Quaternion.Euler(0, 0, 1 * subAngle * (i + 1));
+                if (IsLookPlayer(angleLeft) || IsLookPlayer(angleRight))
+                {
+                    _isFoundPlayer = true;
+                }
             }
 
-            return false;
+            //射出射线检测是否有Player
+            bool IsLookPlayer(Quaternion eulerAnger)
+            {
+                var hit = Physics2D.Raycast(transform.position, eulerAnger * transform.up.normalized * _lookDistance,
+                    _lookDistance, _layerMask);
+                return hit.collider != null && hit.transform.CompareTag("Player");
+            }
         }
 
-        //射出射线检测是否有Player
-        public bool LookAround(Quaternion eulerAnger)
+        private void EnemyDetectPerSec()
         {
-            Debug.DrawRay(transform.position, eulerAnger * transform.up.normalized * _lookDistance, Color.cyan);
-            return Physics.Raycast(transform.position, eulerAnger * transform.up.normalized * _lookDistance,
-                out var hit,
-                _lookDistance) && hit.collider.CompareTag("Player");
+            if (_isFoundPlayer)
+            {
+                _currentAlertValue += _addPerSecondAlertValue;
+            }
+            else
+            {
+                _currentAlertValue += _reducePerSecondAlertValue;
+            }
+
+            _currentAlertValue = Mathf.Clamp(_currentAlertValue, 0, _maxAlertValue);
+            TypeEventSystem.Global.Send(new GameEnemyAlertEvt(EnemyId, _currentAlertValue));
+            if (_currentAlertValue >= _maxAlertValue)
+            {
+                TypeEventSystem.Global.Send(new GameOverEvt());
+            }
         }
     }
 }
--- Unity/Assets/Scripts/GamePlay/GameRoot.cs
+++ Unity/Assets/Scripts/GamePlay/GameRoot.cs
@@ -1,7 +1,4 @@
-using System.Linq;
-using Game.GameEvent;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 namespace Game
 {
@@ -9,27 +6,8 @@ namespace Game
     {
         private void Awake()
         {
-            GameDataCache.Instance.EnemyList = GameObject.FindObjectsOfType<EnemyController>().ToList();
-            GameDataCache.Instance.Player = GameObject.FindObjectOfType<PlayerController>();
-
-            TypeEventSystem.Global.Register<GameOverEvt>(OnGameOverEvt);
-        }
-
-
-        private void OnDestroy()
-        {
-            TypeEventSystem.Global.UnRegister<GameOverEvt>(OnGameOverEvt);
-        }
-
-        private void OnGameOverEvt(GameOverEvt obj)
-        {
-            ReloadScene();
-        }
-
-
-        public void ReloadScene()
-        {
-            SceneManager.LoadScene(GameDataCache.Instance.CrtSceneIdx);
+            Application.targetFrameRate = 60;
+            DontDestroyOnLoad(this.gameObject);
         }
     }
 }
