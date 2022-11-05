b653f80d441f4c37a934ecfe1270e737a1a516e0
Null <289040411@qq.com>
2022-11-05 14:07:34 +0800

    单例类

--- Unity/Assets/Scripts/GamePlay/Enemy.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Game
-{
-    public class Enemy : MonoBehaviour
-    {
-        
-    }
-}
---
+++ Unity/Assets/Scripts/GamePlay/EnemyController.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace Game
+{
+    public class EnemyController : MonoBehaviour
+    {
+        public Light2D SightLight2D;
+
+        private void Awake()
+        {
+            
+        }
+        
+        
+    }
+}
