438fb09cb206cb259a42001475a7ffd6452f89c3
Berall0 <2823963140@qq.com>
2022-11-05 13:53:25 +0800

    预制体

--- Unity/Assets/Scripts/PlayeMove.cs
+++ Unity/Assets/Scripts/PlayeMove.cs
@@ -9,16 +9,20 @@ namespace Game
         private float x;
         private float y;
         [SerializeField]private float moveSpeed;
+        private Vector2 mouseV2;
 
         #region 组件
         private Rigidbody2D rb;
         private Collider2D cld;
+        [SerializeField] private GameObject gunGo;
+        private Camera _camera;
         #endregion
         
         
         // Start is called before the first frame update
         void Start()
         {
+            _camera = Camera.main;
             rb = GetComponent<Rigidbody2D>();
             cld = GetComponent<Collider2D>();
         }
@@ -26,8 +30,10 @@ namespace Game
         // Update is called once per frame
         void Update()
         {
+            mouseV2 = _camera.ScreenToWorldPoint(Input.mousePosition);
             x = Input.GetAxis("Horizontal");
             y = Input.GetAxis("Vertical");
+            
             
 
             rb.velocity = new Vector2(x*moveSpeed, y*moveSpeed);
