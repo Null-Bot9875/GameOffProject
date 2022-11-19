3169de08985e6e1ebce59bba065284a06ec7ff05
Null <289040411@qq.com>
2022-11-19 21:44:05 +0800

    简化代码

--- Unity/Assets/Scripts/GameUtility/GamePath.cs
+++ Unity/Assets/Scripts/GameUtility/GamePath.cs
@@ -3,7 +3,9 @@
     public static class GamePath
     {
         public static string PrefabPath = "Prefabs/";
+        public static string AnimationPath = "Animation/";
         public static string BulletPath = PrefabPath + "Item/Bullet";
         public static string BulletOnwallPath = PrefabPath + "Item/BulletOnwall";
+        public static string GunClipPath = AnimationPath + "Gun";
     }
 }
