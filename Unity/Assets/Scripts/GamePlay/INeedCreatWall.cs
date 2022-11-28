28db05d6e1246e8e8bb1cbc882487b7cb514067b
Null <289040411@qq.com>
2022-11-28 11:58:05 +0800

    Update INeedCreatWall.cs

--- Unity/Assets/Scripts/GamePlay/INeedCreatWall.cs
+++ Unity/Assets/Scripts/GamePlay/INeedCreatWall.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Game
@@ -10,30 +6,30 @@ namespace Game
     {
         public bool 是否是固定外墙;
         public PhysicsMaterial2D P2D;
-        
+
         private void Awake()
         {
             if (gameObject.GetComponent<Rigidbody2D>() != null)
             {
                 return;
-                
             }
+
             if (是否是固定外墙)
             {
-                gameObject.AddComponent<BoxCollider2D>();
+                var collider = gameObject.AddComponent<BoxCollider2D>();
+                collider.size = new Vector2(1.2f, 1.2f);
                 gameObject.AddComponent<WallCtr>();
                 gameObject.layer = LayerMask.NameToLayer("Wall");
                 gameObject.isStatic = true;
             }
             else
             {
-                gameObject.AddComponent<BoxCollider2D>();
-                gameObject.AddComponent<Rigidbody2D>();
-                Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+                var rb = gameObject.AddComponent<Rigidbody2D>();
                 rb.bodyType = RigidbodyType2D.Static;
                 rb.sharedMaterial = P2D;
                 gameObject.isStatic = true;
             }
+
             Destroy(this);
         }
     }
